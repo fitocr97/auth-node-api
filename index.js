50695501ff14a52e5d61655acfca36ddcde0ffa2
Rodolfo Mora <rodolfomora97@gmail.com>
2024-01-30 11:05:10 -0600

    login

--- index.js
+++ index.js
@@ -50,6 +50,29 @@ app.post('/register', async (req, res) => {
     }
 })
 
+
+app.post('/login', async (req, res) => {
+    const { body } = req
+    try {
+        const user = await User.findOne({email: body.email})
+        if (!user) {
+            res.status(403).send('usuario y/o contraseña incorrecto')
+        }else{
+            const isMatch = await bcrypt.compare(body.password, user.password)
+            if (isMatch) {
+                const signed = signToken(user._id)
+                res.status(200).send(signed)
+            }else{
+                res.status(403).send('usuario y/o contraseña incorrecto')
+            }
+        }
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+
+
 app.listen(3000, () => {
     console.log('Iniciando el servidor, port 3000')
 })
