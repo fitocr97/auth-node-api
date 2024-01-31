28dc02e6a7eade5c414cd41719df64bf1073e040
Rodolfo Mora <rodolfomora97@gmail.com>
2024-01-31 16:23:42 -0600

    auth node postman

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const expressJwt = require('express-jwt')
-const User = require('./user')
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {expressjwt:expressJwt} = require('express-jwt');
+const User = require('./user');
 
 require('dotenv').config();
 
@@ -12,20 +12,23 @@ const dbPassword = process.env.DB_PASSWORD;
 const dbUser = process.env.DB_USER;
 const stringSecreto = process.env.JWT_STRING;
 
-//conexion db
+// Conexion a la base de datos
 try {
-    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.rihd75b.mongodb.net/auth?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.rihd75b.mongodb.net/auth?retryWrites=true&w=majority`);
     console.log('Conexión a la base de datos exitosa');
 } catch (error) {
-    console.error('Error al conectar a la base de datos:', error)
+    console.error('Error al conectar a la base de datos:', error);
 }
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+//validar si esta logueado
+const validateJWT = expressJwt({ secret: stringSecreto, algorithms: ['HS256'] }); // Middleware
+//token loguear
+const signToken =  _id => jwt.sign({ _id }, stringSecreto);
 
 //registrar un usuario en la bd
-const signToken =  _id => jwt.sign({ _id }, stringSecreto)
 app.post('/register', async (req, res) => {
     const {body} = req
     console.log({body})
@@ -50,7 +53,7 @@ app.post('/register', async (req, res) => {
     }
 })
 
-
+//login
 app.post('/login', async (req, res) => {
     const { body } = req
     try {
@@ -71,8 +74,48 @@ app.post('/login', async (req, res) => {
     }
 })
 
+//
+const findAndAssignUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.auth._id)
+        if(!user){
+            return res.status(401).end() //jwt invalido
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+const isAuthenticated = express.Router().use(validateJWT, findAndAssignUser)
+
+//validar jwt y autorizacio funciona
+/*
+app.get('/lele', validateJWT,(req, res, next) =>{
+    console.log(validateJWT)
+    console.log('lala' , req.auth)
+    res.send('ok')
+})*/
 
 
+app.get('/lele', isAuthenticated, (req, res) =>{
+    //throw new Error('errorrrrr) //error
+    res.send(req.user)
+})
+
+//manejo errores (error request response next)  
+app.use((err, req, res, next) =>{
+    console.error('error nuevo')
+    next(err)
+})
+
+app.use((err, req, res, next) =>{
+    res.sen('Ha ocurrido un error') //se podria devolver un html  custom para mostrar
+})
+
+
+//servidor
 app.listen(3000, () => {
     console.log('Iniciando el servidor, port 3000')
 })
